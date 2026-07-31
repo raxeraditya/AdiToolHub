1f566ef6cb73f55665cb536c9bc4d91d3b0bd171
raxeraditya <brahmaditya@hotmail.com>
2026-07-31 10:48:57 +0530

    go to old version

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+};
+
+module.exports = nextConfig;
